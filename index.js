da4b1c879dfb56d87c268c66c544991a1c03f8e1
Madison Ciccarelli <mciccadesign@gmail.com>
2023-09-23 14:29:58 -0700

    start in JS and finalized layout

---
+++ index.js
@@ -0,0 +1,21 @@
+
+selectedImages.addEventListener("click", selected({
+
+});
+
+function selected(){
+    let selectedImages = document.querySelector(".img-gallery")
+
+
+}
+
+
+function winCounter(){
+
+}
+
+
+
+
+switchImage()
+winCounter()
