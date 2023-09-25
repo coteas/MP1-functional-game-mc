fb2f4f48a371115b759501419a371912a7a9a0a4
Madison Ciccarelli <mciccadesign@gmail.com>
2023-09-24 20:22:02 -0700

    Stuck on JS conditional statement

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-let selectedImages = document.querySelectorAll(".combo-1, .combo-2, .combo-3")
+let selectedItemAudio = new Audio('assets/selected-button.mp3')
+let correctSelectAudio = new Audio('assets/correct-chime.mp3')
+let incorrectSelectAudio = new Audio('assets/negative-beep.mp3')
+let failureAudio = new Audio('assets/negative-beep.mp3')
+let successAudio = new Audio('assets/success-bell.mp3')
+
+let selectedImages = document.querySelector(".item-gallery")
+let correctAnswer = true
+let wrongAnswer = true
+
+
 document.addEventListener('click', function selected(evt) {
 evt.target.classList.toggle('selected');
+selectedItemAudio.play();
   });  
 
+  document.addEventListener('click', function firstCombo(evt) {
+
+      }); 
+  
+function thatIsIncorrect(){
+
+}
+
+function firstCombo(e){
+let aClass = document.querySelectorAll(".combo-1a, .combo-1b")
+aClass = correctAnswer
+if(correctAnswer){
+   aClass.forEach(image =>{
+    image.classList.add('correct-answer')
+   })
+}else{
+    e.classList.add('wrong-answer')
+}
+}
+
+function secondCombo(e){
+
+}
+
+function thirdCombo(e){
+
+}
 
 function selected(){
 selectedImages.forEach(image =>{
     image.classList.add('selected')
 })
-
 }
-
-function winCounter(){
-
-}
-
-
-
-
-winCounter()
