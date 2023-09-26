e6e34ccea1da6115c24a0d1b6ac8dfb3505eae35
Madison Ciccarelli <mciccadesign@gmail.com>
2023-09-25 20:57:29 -0700

    More trouble shooting

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let correctSelectAudio = new Audio('assets/correct-chime.mp3')
 let incorrectSelectAudio = new Audio('assets/negative-beep.mp3')
 let failureAudio = new Audio('assets/negative-beep.mp3')
 let successAudio = new Audio('assets/success-bell.mp3')
-
 let selectedImages = document.querySelector(".item-gallery")
 
 for(let i=0; i < selectedImages.children.length; i++){
@@ -18,11 +17,16 @@ let aClass = document.querySelectorAll(".combo-1a > img.selected, .combo-1b > im
 if(aClass.length == 2){
 aClass.forEach(image =>{
 image.classList.add('correct-answer')
+correctSelectAudio.play();
 })
 }else if(aClass.length == 1){
-    
-}else{
-e.target.classList.add('wrong-answer')
+    let currentSelected = document.querySelectorAll('.selected')
+    if(document.querySelectorAll('.selected').length > 1) {
+        for(let i=0;i < currentSelected.length; i++){
+            currentSelected[i].classList.add('wrong-answer')
+            incorrectSelectAudio.play();
+        }
+    }
 }
 });
 
@@ -31,25 +35,35 @@ document.querySelector(".combo-1b").addEventListener('click',  (e)=> {
     if(aClass.length == 2){
     aClass.forEach(image =>{
     image.classList.add('correct-answer')
+    correctSelectAudio.play();
     })
 }else if(aClass.length == 1){
-        
-}else{
-    e.target.classList.add('wrong-answer')
+    let currentSelected = document.querySelectorAll('.selected')
+if(document.querySelectorAll('.selected').length > 1) {
+        for(let i=0;i < currentSelected.length; i++){
+            currentSelected[i].classList.add('wrong-answer')
+            incorrectSelectAudio.play();
+        }
     }
-    });
+}
+});
 
 document.querySelector(".combo-2a").addEventListener('click',  (e)=> {
     let bClass = document.querySelectorAll(".combo-2a > img.selected, .combo-2b > img.selected")
     if(bClass.length == 2){
     bClass.forEach(image =>{
     image.classList.add('correct-answer')
+    correctSelectAudio.play();
     })
-    }else if(bClass.length == 1){
-
-    }else{
-    e.target.classList.add('wrong-answer')
-    }
+}else if(bClass.length == 1){
+    let currentSelected = document.querySelectorAll('.selected')
+    if(document.querySelectorAll('.selected').length > 1) {
+            for(let i=0;i < currentSelected.length; i++){
+                currentSelected[i].classList.add('wrong-answer')
+                incorrectSelectAudio.play();
+            }
+        }
+}
     });
 
 document.querySelector(".combo-2b").addEventListener('click',  (e)=> {
@@ -57,36 +71,62 @@ document.querySelector(".combo-2b").addEventListener('click',  (e)=> {
     if(bClass.length == 2){
     bClass.forEach(image =>{
     image.classList.add('correct-answer')
+    correctSelectAudio.play();
     })
-  }else if(bClass.length == 1){
-        
-    }else{
-    e.target.classList.add('wrong-answer')
-    }
+}else if(bClass.length == 1){
+    let currentSelected = document.querySelectorAll('.selected')
+    if(document.querySelectorAll('.selected').length > 1) {
+            for(let i=0;i < currentSelected.length; i++){
+                currentSelected[i].classList.add('wrong-answer')
+                incorrectSelectAudio.play();
+            }
+        }
+}
     });
 
 document.querySelector(".combo-3a").addEventListener('click',  (e)=> {
     let cClass = document.querySelectorAll(".combo-3a > img.selected, .combo-3b > img.selected")
-    if(cClass.length == 2){
-    cClass.forEach(image =>{
-    image.classList.add('correct-answer')
-    })
+if(cClass.length == 2){
+        cClass.forEach(image =>{
+        image.classList.add('correct-answer')
+        correctSelectAudio.play();
+        })
 }else if(cClass.length == 1){
-        
-}else{
-    e.target.classList.add('wrong-answer')
+    let currentSelected = document.querySelectorAll('.selected')
+if(document.querySelectorAll('.selected').length > 1) {
+        for(let i=0;i < currentSelected.length; i++){
+            currentSelected[i].classList.add('wrong-answer')
+            incorrectSelectAudio.play();
+        }
     }
+}
     });
 
 document.querySelector(".combo-3b").addEventListener('click', (e)=> {
     let cClass = document.querySelectorAll(".combo-3a > img.selected, .combo-3b > img.selected")
-    if(cClass.length == 2){
-    cClass.forEach(image =>{
-    image.classList.add('correct-answer')
-    })
+if(cClass.length == 2){
+        cClass.forEach(image =>{
+        image.classList.add('correct-answer')
+        correctSelectAudio.play();
+        })     
 }else if(cClass.length == 1){
-        
-}else{
-    e.target.classList.add('wrong-answer')
+    let currentSelected = document.querySelectorAll('.selected')
+if(document.querySelectorAll('.selected').length > 1) {
+        for(let i=0;i < currentSelected.length; i++){
+            currentSelected[i].classList.add('wrong-answer')
+            incorrectSelectAudio.play();
+        }
     }
-    });
+    }
+});
+
+
+function youWin(){
+
+}
+
+
+function youLose(){
+
+
+}
